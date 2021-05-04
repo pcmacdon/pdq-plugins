3dfd3ae5a417a6a495d3e2642f6a5609e711356a
pmacdona <>
2021-05-04 20:31:07 +0000

    Add initial plugins/jsi_config.js
    FossilOrigin-Name: b530c6af9a6f01cf2991dc0a63dbe19273de7ecf0342b9bbb744f54d505350c4

---
+++ plugins/jsi_config.js
@@ -0,0 +1,20 @@
+window.PdqConfigStr = `{
+  "builtins":[ {
+      "date":"2021-05-04", 
+      "enabled":false, 
+      "hide":false, 
+      "name":"admin", 
+      "uionly":false, 
+      "vuecomps":""
+    } ], 
+  "confattrs":{
+    "histmode":false, 
+    "home":"/admin", 
+    "noBreak":false
+  }, 
+  "plugins":[], 
+  "status":{
+    "projcode":"76942adb34820dd6975b0aabf39969eb3589d32a", 
+    "project":"PDQ-PLUGINS"
+  }
+}`;
