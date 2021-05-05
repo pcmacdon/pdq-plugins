19f30dd20fa492069210999445df56fd3926a815
pmacdona <>
2021-05-05 16:28:22 +0000

    Config update content.json
    FossilOrigin-Name: d86301c8fbe08d48daf735da02842f0438a2a54e3cd0c3c3ed4258ff8bd65d6f

--- plugins/jsi_config.js
+++ plugins/jsi_config.js
@@ -12,7 +12,28 @@ window.PdqConfigStr = `{
     "home":"/admin", 
     "noBreak":false
   }, 
-  "plugins":[], 
+  "plugins":[ {
+      "date":"2021-05-05", 
+      "enabled":true, 
+      "hide":false, 
+      "name":"demo_tabs", 
+      "uionly":false, 
+      "vuecomps":""
+    }, {
+      "date":"2021-05-05", 
+      "enabled":true, 
+      "hide":false, 
+      "name":"posts", 
+      "uionly":false, 
+      "vuecomps":""
+    }, {
+      "date":"2021-05-05", 
+      "enabled":true, 
+      "hide":false, 
+      "name":"video", 
+      "uionly":false, 
+      "vuecomps":""
+    } ], 
   "status":{
     "projcode":"76942adb34820dd6975b0aabf39969eb3589d32a", 
     "project":"PDQ-PLUGINS"
